00e984c22ed20e8b1ba9191946c9ed5255035a02
yuri_regulus_25 <guxtutirinren@gmail.com>
2026-02-22 19:43:05 +0900

    サンプル+C#導通確認用ファイル配置

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-card>
+      <v-card-title>サンプルだよぉ</v-card-title>
+      <v-card-text>
+        C#との通信を確認します
+        <v-btn @click="callBackendMethods()">呼んでみる?</v-btn>
+      </v-card-text>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
     //
   }),
+
+  methods: {
+    async callBackendMethods() {
+      const bridge = window.chrome.webview.hostObjects.bridge; 
+      await bridge.MsgBox("引数だよぉ");
+    }
+  }
 };
 </script>
